32443aa84edbcd9cc3ad7250e4623a28fb7eae4e
dumindapriyasad <duminda.priyasad@gmail.com>
2023-06-20 08:13:00 +0530

    List Products

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,18 @@
 // const inter = Inter({ subsets: ['latin'] });
 
 import Layout from '@/components/Layout';
+import ProductItem from '@/components/ProductItem';
+import data from '@/utils/data';
 
 export default function Home() {
-  return <Layout title="Home Page">Home page</Layout>;
+  return (
+    <Layout title="Home Page">
+      {/* Load product thumbnails */}
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
+    </Layout>
+  );
 }
