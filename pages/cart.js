2ba3d58ffba9625efa976af03eb889ad7130a71e
dumindapriyasad <duminda.priyasad@gmail.com>
2023-06-28 18:49:31 +0530

    Update Quantity in the Cart

--- pages/cart.js
+++ pages/cart.js
@@ -16,9 +16,15 @@ const CartScreen = () => {
     cart: { cartItems },
   } = state;
 
-  // Remove cart item
+  // Remove cart items
   const removeItemHandler = (item) => {
     dispatch({ type: 'CART_REMOVE_ITEM', payload: item });
+  };
+
+  // Update cart items
+  const updateCartHandler = (item, qty) => {
+    const quantity = Number(qty);
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...item, quantity } });
   };
 
   return (
@@ -42,6 +48,7 @@ const CartScreen = () => {
                 </tr>
               </thead>
 
+              {/* Cart items */}
               <tbody>
                 {cartItems.map((item) => (
                   <tr key={item.slug} className="border-b">
@@ -64,8 +71,24 @@ const CartScreen = () => {
                         {item.name}
                       </Link>
                     </td>
-                    <td className="p-5 text-right">{item.quantity}</td>
+
+                    <td className="p-5 text-right">
+                      <select
+                        value={item.quantity}
+                        onChange={(e) =>
+                          updateCartHandler(item, e.target.value)
+                        }
+                      >
+                        {[...Array(item.countInStock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))}
+                      </select>
+                    </td>
+
                     <td className="p-5 text-right">${item.price}</td>
+
                     <td className="p-5 text-center">
                       <button onClick={() => removeItemHandler(item)}>
                         <XCircleIcon className="h-5 w-5"></XCircleIcon>
